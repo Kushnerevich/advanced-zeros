48567b3483bc19a92f4b319cb4b9ef54bf4fdd44
pavel <pasha2701@tut.by>
2018-03-11 13:36:59 +0300

    Did 7th task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var bases = [];
+    var a = 2;
+    getPrimitives(base);
+    bases.reverse();
+
+    var j=1;
+    result =[];
+
+    for (i = 0; i < bases.length; i++)
+    {
+        var sum = 0;
+        var n = number;
+        while (n >= 1) {
+            var temp = parseInt(n / bases[i]);
+            n = temp;
+            sum = sum + temp;
+
+        }
+        if (bases[i]==bases[i+1])
+        {
+            j++;
+        }
+        result.push(Math.floor(sum / j));
+
+    }
+
+    function getPrimitives(base) {
+        while (a <= base) {
+            if (base % a == 0) {
+                bases.push(a);
+                base = base / a;
+            }
+            else {
+                a++
+            }
+            ;
+        }
+    }
+
+    return Math.min.apply(null, result);
+
+
 }
